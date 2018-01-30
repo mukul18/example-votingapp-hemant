05428f452b3740b4e4843a30a03db51db5af232a
hemantkbajaj <hemantkbajaj@gmail.com>
2018-01-30 11:30:31 +0530

    123
    123

--- vote/app.py
+++ vote/app.py
@@ -6,7 +6,7 @@ import random
 import json
 
 option_a = os.getenv('OPTION_A', "Mukul")
-option_b = os.getenv('OPTION_B', "Arun")
+option_b = os.getenv('OPTION_B', "Arun123")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
